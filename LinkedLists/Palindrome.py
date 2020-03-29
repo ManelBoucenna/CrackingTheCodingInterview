a9ae3aea077e608a1b45d27aa7e718d41fbb2730
Manal Maroua Boucenna <manal_maroua.boucenna@g.enp.edu.dz>
2020-03-29 12:17:18 -0400

    Update Palindrome.py

--- LinkedLists/Palindrome.py
+++ LinkedLists/Palindrome.py
@@ -22,13 +22,23 @@ class SLinkedList:
         if (self.headval == None):
             self.headval = newNode
             return
+        newNode.nextval = self.headval
+        self.headval = newNode
+       
+       
+    def isPalindromeWithString(self):# O(n)
         currentNode = self.headval
-        while currentNode.nextval is not None:
+        s = ""
+        while (currentNode is not None):
+            s+=currentNode.dataval
             currentNode = currentNode.nextval
-        currentNode.nextval = newNode
-        self.size += 1
-       
-       
+        if (s!=s[::-1]):
+            print("This is not a palindrome")
+            return False
+        else:
+            print("This is a palindrome")
+            return True
+            
     def isPalindromeWithStack(self):# O(n)
         currentNode = self.headval
         stack = []
@@ -52,21 +62,41 @@ class SLinkedList:
             ReversedList.Preppend(currentNode.dataval)
             currentNode = currentNode.nextval
         currentNode = self.headval
+        ReversedNode = ReversedList.headval
         while (currentNode is not None):
-            val = stack.pop()
-            if (currentNode.dataval != val):
+            if (currentNode.dataval != ReversedNode.dataval):
                 print("This is not a palindrome")
                 return False
             currentNode = currentNode.nextval
+            ReversedNode = ReversedNode.nextval
+        print("This is a palindrome")
+        return True
+        
+    def isPalindromeWithDoublePointers(self):# O(n)
+        p = self.headval
+        q = self.headval
+        prevs = []
+        i=0
+        while(q):
+            prevs.append(q)
+            q=q.nextval
+            i+=1   
+        count = 1
+        while (count<=i//2+1):
+            if (p.dataval != prevs[-count].dataval):
+                print("This is not a palindrome")
+                return False
+            p = p.nextval
+            count+=1
         print("This is a palindrome")
         return True
     
 List = SLinkedList()
 
-List.Append("M")
+List.Append("K")
 List.Append("A")
 List.Append("Y")
 List.Append("A")
 List.Append("K")
 
-List.isPalindromeWithStack()
+List.isPalindromeWithDoublePointers()

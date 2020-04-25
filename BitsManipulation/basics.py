789992150418dceddb83d0ad91fb930acc015ccf
Manal Maroua Boucenna <manal_maroua.boucenna@g.enp.edu.dz>
2020-04-25 09:29:49 -0400

    Create basics.py

---
+++ BitsManipulation/basics.py
@@ -0,0 +1,13 @@
+def GetBit(position):
+    pass
+
+def SetBit(Position):
+    pass
+
+def ClearBit(position):
+    pass
+
+def ClearMSBtoPosition(position):# Clear from most significant bit to position
+    pass
+def ClearPositionToZero(position):
+    pass

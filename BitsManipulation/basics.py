1218835069b17fad16bc77a084c2b6732342b89e
Manal Maroua Boucenna <manal_maroua.boucenna@g.enp.edu.dz>
2020-04-25 10:08:47 -0400

    Update basics.py

--- BitsManipulation/basics.py
+++ BitsManipulation/basics.py
@@ -1,13 +1,30 @@
-def GetBit(position):
-    pass
+def GetBit(num, position):
+    return (num & (1<<position))!=0
 
-def SetBit(Position):
-    pass
+def SetBit(num, position):
+    return num | (1<<position)
 
-def ClearBit(position):
-    pass
+def ClearBit(num, position):
+    mask = ~(1<<position)
+    return num & mask
 
-def ClearMSBtoPosition(position):# Clear from most significant bit to position
-    pass
-def ClearPositionToZero(position):
-    pass
+def ClearMSBtoPosition(num, position):# Clear from most significant bit to position
+    #Ex: num = 11010, pos = 2 => num = 00010
+    #    mask= 00011 = 11100 -1
+    mask = (1<<position) -1 
+    return num & mask
+
+def ClearPositionToZero(num, position):
+    #Ex: num = 11010, pos = 1 => num = 11000
+    #    mask= 11100 = 1111<<2 = -1<<2
+    mask = -1<<(position+1)
+    return num & mask
+    
+def UpdateBit(num, pos, val):
+    #Ex: num = 11010, pos = 1, val=0 => num = 11000
+    #    mask= 11101 = ~00010
+    value = 1 if val else 0
+    mask = ~(1<<pos)
+    return (num  & mask)|(val<<pos)
+    
+UpdateBit(8,0,True)

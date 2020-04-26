0e8d115dc2f11df022933a6e801c6f27e2ab4a08
Manal Maroua Boucenna <manal_maroua.boucenna@g.enp.edu.dz>
2020-04-26 09:42:18 -0400

    Update BinaryToString.py

--- BitsManipulation/BinaryToString.py
+++ BitsManipulation/BinaryToString.py
@@ -1,8 +1,19 @@
 def BinaryToString(num):
     # Case:
-    #num = 0.5 
+    #num = 0.75 =0.11
+    bit_printed = 1
+    bits = ["."]
+    while(num>0):
+        r = num*2
+        if (r>=1):
+            bits.append("1")
+            num = r - 1
+        else:
+            bits.append("0")
+            num = r
+        if (bit_printed>31):
+            return ("Error!")
+        bit_printed +=1
+    print(bits)
 
-    pass
-
-
-BinaryToString(0.5)
+BinaryToString(0.96)
